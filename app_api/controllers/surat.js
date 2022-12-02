ee15f0ed71b65cb2bf270816f9fe7d6d0321d4ea
ahmfarisi <ahmfarisi@gmail.com>
2022-12-02 19:41:08 +0700

    change api response

--- app_api/controllers/surat.js
+++ app_api/controllers/surat.js
@@ -1,79 +1,76 @@
-const mongoose = require ("mongoose");
+const mongoose = require("mongoose");
 
 const surat = mongoose.model("Surat");
 
-const suratList = (req,res) => {
+const suratList = (req, res) => {
     surat.find({},
-        function(err, result) {
+        function (err, result) {
             if (err) {
                 res.status(500)
-                    .json({"status" : "failed"});
+                    .json({
+                        "status": "failed"
+                    });
             } else {
                 res.status(200)
-                .json(
-                    {
-                        "status" : "success",
-                        "data" : result
-                    });
+                    .json(result);
             }
         });
 
 }
 
-const suratCreate = (req,res) => {
-    surat.create(
-        {
-            nomor_surat:req.body.nomor_surat,
-            tgl_surat: req.body.tgl_surat,
-            jenis_surat : req.body.jenis_surat,
-            uraian : req.body.uraian,
-            pejabat_penandatangan:req.body.pejabat_penandatangan,
-            nama_penandatangan:req.body.nama_penandatangan,
-            nama_ditugaskan : req.body.nama_ditugaskan
-        }, (err, result) => {
-            if (err) {
-                res
+const suratCreate = (req, res) => {
+    surat.create({
+        nomor_surat: req.body.nomor_surat,
+        tgl_surat: req.body.tgl_surat,
+        jenis_surat: req.body.jenis_surat,
+        uraian: req.body.uraian,
+        pejabat_penandatangan: req.body.pejabat_penandatangan,
+        nama_penandatangan: req.body.nama_penandatangan,
+        nama_ditugaskan: req.body.nama_ditugaskan
+    }, (err, result) => {
+        if (err) {
+            res
                 .status(400)
                 .json(err);
-            } else {
-                res
+        } else {
+            res
                 .status(201)
                 .json(result);
-            }
-        });
+        }
+    });
 
 }
 
-const suratReadOne = (req,res) => {
+const suratReadOne = (req, res) => {
     surat
         .findById(req.params.id)
-        .exec((err,surat) => {
+        .exec((err, surat) => {
             res
-            .status(200)
-            .json(surat);
+                .status(200)
+                .json(surat);
         });
 }
 
-const suratUpdateOne = (req,res) => {
+const suratUpdateOne = (req, res) => {
     surat
         .findById(req.params.id)
-        .exec((err,surat) => {
+        .exec((err, surat) => {
             surat.nama_surat = req.body.nama_surat;
             surat.tgl_surat = req.body.tgl_surat;
             surat.jenis_surat = req.body.jenis_surat,
-            surat.uraian = req.body.uraian,
-            surat.pejabat_penandatangan = req.body.pejabat_penandatangan,
-            surat.nama_penandatangan = req.body.nama_penandatangan,
-            surat.nama_ditugaskan = req.body.nama_ditugaskan
+                surat.uraian = req.body.uraian,
+                surat.pejabat_penandatangan = req.body.pejabat_penandatangan,
+                surat.nama_penandatangan = req.body.nama_penandatangan,
+                surat.nama_ditugaskan = req.body.nama_ditugaskan
             surat.save((err, result) => {
                 if (err) {
                     res
-                    .status(404)
-                    .json(err);
+                        .status(404)
+                        .json(err);
                 } else {
                     res
-                    .status(200)
-                    .json(result);
+                        .status(200)
+                        .json(result);
                 }
             });
 
@@ -82,19 +79,19 @@ const suratUpdateOne = (req,res) => {
 
 }
 
-const suratDeleteOne = (req,res) => {
+const suratDeleteOne = (req, res) => {
     surat
         .findById(req.params.id)
-        .exec((err,surat) => {
-            surat.remove((err,result) => {
+        .exec((err, surat) => {
+            surat.remove((err, result) => {
                 if (err) {
                     res
-                    .status(404)
-                    .json(null);
-                } 
+                        .status(404)
+                        .json(null);
+                }
                 res
-                .status(204)
-                .json(null);
+                    .status(204)
+                    .json(null);
             });
         });
 }
